9659c84866ad7db0918e4c4520e7cf1025c6b1ae
Katlyn <katlyn880815@gmail.com>
2023-11-26 21:42:12 +0800

    chore: install fontAwsome to use its icon library

--- src/app/mylibrary/mybooks/page.tsx
+++ src/app/mylibrary/mybooks/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 
 import React, { useEffect } from "react";
+import styles from "./page.module.css";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
 import Spinner from "@/components/Spinner/Spinner";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Mybooks() {
   const { isLogin, pending } = useAuth();
   const router = useRouter();
 
-  useEffect(() => {
-    if (!isLogin) router.push("/");
-  }, [isLogin, router]);
-
-  return <>{pending ? <Spinner /> : <div>My books</div>}</>;
+  return (
+    <>
+      <main className={styles.main}>
+        <FontAwesomeIcon icon={faEnvelope} className={styles.icon} />
+      </main>
+    </>
+  );
 }
