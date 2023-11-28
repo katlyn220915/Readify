dae7693a2b0fcc71bc6c571433805dd6e5ff9593
Katlyn <katlyn880815@gmail.com>
2023-11-28 12:24:46 +0800

    fix: topbar component, fix the typo of import css file

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import styles from "./topbar.module.css";
+import styles from "./Topbar.module.css";
 import { usePathname } from "next/navigation";
 
 import Icon from "@/components/Icon/Icon";
