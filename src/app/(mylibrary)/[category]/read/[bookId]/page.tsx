8a841344e13f3e691f14615f6ca09fcca3bd75b9
Katlyn <katlyn880815@gmail.com>
2024-01-22 11:50:15 +0800

    style: ReadingArea, ReadingNavigation, ContentsRoot, No teBook, RWD for these components.

--- src/app/(mylibrary)/[category]/read/[bookId]/page.tsx
+++ src/app/(mylibrary)/[category]/read/[bookId]/page.tsx
@@ -7,28 +7,66 @@ import ReadingArea from "@/components/ReadingArea/ReadingArea";
 import ReadingNavigation from "@/components/ReadingNavigation/ReadingNavigation";
 import Notebook from "@/components/Notebook/Notebook";
 import BookIndices from "@/components/BookIndices/BookIndices";
+import { useRWD } from "@/hooks/useRWD/useRWD";
 
 export default function BookId() {
-  const [isNotebookOpen, setIsNotebookOpen] = useState(true);
-  const [isContentListOpen, setIsContentListOpen] = useState(true);
-  const [isNavigationVisible, setIsNavigationVisible] = useState(true);
+  const [isNotebookOpen, setIsNotebookOpen] = useState(false);
+  const [isContentListOpen, setIsContentListOpen] = useState(false);
+  const [isNavigationVisible, setIsNavigationVisible] = useState(false);
+  const { screenWidth } = useRWD();
 
   // console.log("render -", " BookId page");
 
   return (
     <>
-      <ReadingNavigation
-        isContentListOpen={isContentListOpen}
-        onSetContentListOpen={setIsContentListOpen}
-        isNotebookOpen={isNotebookOpen}
-        onSetNotebookOpen={setIsNotebookOpen}
-        isNavigationVisible={isNavigationVisible}
-      />
-      <div className={`${styles.container}`}>
-        <BookIndices isContentListOpen={isContentListOpen} />
-        <ReadingArea setIsNavigationVisible={setIsNavigationVisible} />
-        <Notebook isNotebookOpen={isNotebookOpen} />
-      </div>
+      {screenWidth > 1024 && (
+        <ReadingNavigation
+          isContentListOpen={isContentListOpen}
+          onSetContentListOpen={setIsContentListOpen}
+          isNotebookOpen={isNotebookOpen}
+          onSetNotebookOpen={setIsNotebookOpen}
+          isNavigationVisible={isNavigationVisible}
+        />
+      )}
+      {screenWidth < 1024 && !isNotebookOpen && !isContentListOpen && (
+        <ReadingNavigation
+          isContentListOpen={isContentListOpen}
+          onSetContentListOpen={setIsContentListOpen}
+          isNotebookOpen={isNotebookOpen}
+          onSetNotebookOpen={setIsNotebookOpen}
+          isNavigationVisible={isNavigationVisible}
+        />
+      )}
+      {screenWidth > 1024 && (
+        <div className={`${styles.container}`}>
+          <BookIndices
+            isContentListOpen={isContentListOpen}
+            onContentListOpen={setIsContentListOpen}
+          />
+          <ReadingArea setIsNavigationVisible={setIsNavigationVisible} />
+          <Notebook
+            isNotebookOpen={isNotebookOpen}
+            onSetIsNotebookOpen={setIsNotebookOpen}
+          />
+        </div>
+      )}
+      {screenWidth < 1024 && (
+        <div className={`${styles.container}`}>
+          <ReadingArea setIsNavigationVisible={setIsNavigationVisible} />
+          {isContentListOpen && (
+            <BookIndices
+              isContentListOpen={isContentListOpen}
+              onContentListOpen={setIsContentListOpen}
+            />
+          )}
+          {isNotebookOpen && (
+            <Notebook
+              isNotebookOpen={isNotebookOpen}
+              onSetIsNotebookOpen={setIsNotebookOpen}
+            />
+          )}
+        </div>
+      )}
     </>
   );
 }
