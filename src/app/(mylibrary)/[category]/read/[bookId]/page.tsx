fe9d8812e73fc66ea724975c24ef4ae1f7b2a20f
Katlyn <katlyn880815@gmail.com>
2024-01-30 17:32:28 +0800

    feat: protected the routes using redirect and useAuth, when user equal to null, redirect the page to signin

--- src/app/(mylibrary)/[category]/read/[bookId]/page.tsx
+++ src/app/(mylibrary)/[category]/read/[bookId]/page.tsx
@@ -1,19 +1,28 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import styles from "./page.module.css";
+import { redirect } from "next/navigation";
 
 import ReadingArea from "@/components/readingPage/ReadingArea/ReadingArea";
 import ReadingNavigation from "@/components/readingPage/ReadingNavigation/ReadingNavigation";
 import Notebook from "@/components/readingPage/Notebook/Notebook";
 import BookIndices from "@/components/readingPage/BookIndices/BookIndices";
+import { useAuth } from "@/context/AuthContext";
+import Spinner from "@/components/common/Spinner/Spinner";
 
 export default function BookId() {
+  const { user } = useAuth();
   const [isNotebookOpen, setIsNotebookOpen] = useState(false);
   const [isContentListOpen, setIsContentListOpen] = useState(false);
 
+  useEffect(() => {
+    if (user === undefined) return;
+    if (user === null) redirect("/signin");
+  }, [user]);
+
   return (
-    <>
+    <Suspense fallback={<Spinner />}>
       <ReadingNavigation
         isContentListOpen={isContentListOpen}
         onSetContentListOpen={setIsContentListOpen}
@@ -34,6 +43,6 @@ export default function BookId() {
           onSetIsNotebookOpen={setIsNotebookOpen}
         />
       </div>
-    </>
+    </Suspense>
   );
 }
