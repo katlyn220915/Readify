39f608a82d36adf42df115109436bd8f30a5b078
Katlyn <katlyn880815@gmail.com>
2024-01-31 22:11:14 +0800

    chore: page (bookId), seperate read page into differente componentin order to add metadata

--- src/app/(mylibrary)/[category]/read/[bookId]/page.tsx
+++ src/app/(mylibrary)/[category]/read/[bookId]/page.tsx
@@ -1,48 +1,25 @@
-"use client";
+import React from "react";
+import type { Metadata } from "next";
+import Read from "@/components/readingPage/Read/Read";
 
-import React, { Suspense, useEffect, useState } from "react";
-import styles from "./page.module.css";
-import { redirect } from "next/navigation";
+type Props = {
+  params: {
+    category: string;
+    bookId: string;
+  };
+};
 
-import ReadingArea from "@/components/readingPage/ReadingArea/ReadingArea";
-import ReadingNavigation from "@/components/readingPage/ReadingNavigation/ReadingNavigation";
-import Notebook from "@/components/readingPage/Notebook/Notebook";
-import BookIndices from "@/components/readingPage/BookIndices/BookIndices";
-import { useAuth } from "@/context/AuthContext";
-import Spinner from "@/components/common/Spinner/Spinner";
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Book-${params.bookId}`,
+    description: "Read and take notes simultaneously",
+  };
+};
 
-export default function BookId() {
-  const { user } = useAuth();
-  const [isNotebookOpen, setIsNotebookOpen] = useState(false);
-  const [isContentListOpen, setIsContentListOpen] = useState(false);
-
-  useEffect(() => {
-    if (user === undefined) return;
-    if (user === null) redirect("/signin");
-  }, [user]);
-
+export default function Page({ params }: Props) {
   return (
-    <Suspense fallback={<Spinner />}>
-      <ReadingNavigation
-        isContentListOpen={isContentListOpen}
-        onSetContentListOpen={setIsContentListOpen}
-        isNotebookOpen={isNotebookOpen}
-        onSetNotebookOpen={setIsNotebookOpen}
-      />
-      <div className={`${styles.container}`}>
-        <BookIndices
-          isContentListOpen={isContentListOpen}
-          onContentListOpen={setIsContentListOpen}
-        />
-        <ReadingArea
-          isContentListOpen={isContentListOpen}
-          isNotebookOpen={isNotebookOpen}
-        />
-        <Notebook
-          isNotebookOpen={isNotebookOpen}
-          onSetIsNotebookOpen={setIsNotebookOpen}
-        />
-      </div>
-    </Suspense>
+    <>
+      <Read />
+    </>
   );
 }
