2adb5192aa95adbac4e1cdfbce70e2854200afa8
Katlyn <katlyn880815@gmail.com>
2024-01-31 18:13:02 +0800

    feat: page of category, page of root, sign in, sign up, Add metadata for each page, i take the code of category page out of it and made a component for it, in order to add metadata for this page

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,9 +1,14 @@
 import React, { Suspense } from "react";
+import type { Metadata } from "next";
 
 import SignupForm from "@/components/auth/SignupForm/SignupForm";
 import { Header } from "@/components/auth/Header/Header";
 
 import Loading from "../loading";
+
+export const metadata: Metadata = {
+  title: "Sign up",
+};
 
 export default function Signup() {
   return (
